cad9180e52010fbfa100ec1e8fd8b722db529f17
AMIR SADIQ ALI <amirsadiqalisaifi@gmail.com>
2018-09-19 03:16:16 -0700

    defining logic to update todo in indexed db

--- src/indexdb.js
+++ src/indexdb.js
@@ -1,4 +1,3 @@
-
 var db;
 var request = window.indexedDB.open("todoDB", 1);
 
@@ -41,5 +40,38 @@ let remove = id => {
 
   request.onsuccess = function(event) {
     console.log("Todo's entry has been removed from your database.");
+  };
+};
+
+let update = todo => {
+  var request = db
+    .transaction(["todo"], "readwrite")
+    .objectStore("todo")
+    .get(todo.id);
+  request.onsuccess = function(event) {
+    let data = request.result;
+    data.done = !data.done;
+    var updateTitleRequest = db
+      .transaction(["todo"], "readwrite")
+      .objectStore("todo")
+      .put(data);
+
+    // Log the transaction that originated this request
+    console.log(
+      "The transaction that originated this request is " +
+        updateTitleRequest.transaction
+    );
+
+    // When this new request succeeds, run the displayData() function again to update the display
+    updateTitleRequest.onsuccess = function() {
+      console.log("updated Successfully");
+    };
+    console.log("Todo has been added to your database.");
+  };
+
+  request.onerror = function(event) {
+    console.log(
+      "Unable to add data\r\nTodo is aready exist in your database! "
+    );
   };
 };
