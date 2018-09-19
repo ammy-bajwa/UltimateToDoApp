ff30e7f018822b04ea843fc20b4444c362917e3e
Hamza-Yousaf <Hamzayousaf933@gmail.com>
2018-09-19 12:27:41 +0500

    success post test and add post route

--- lib/routes/todoRoutes.ts
+++ lib/routes/todoRoutes.ts
@@ -33,6 +33,37 @@ export class Routes {
           }
       })
       })
-      
-  }
+      .post((req: Request, res: Response) => {
+        if (!req.body.title) {
+          res.status(400).send({ error: "Please Enter a title" });
+        } else if (!req.body.description) {
+          res.status(400).send({ error: "Please Enter a title" });
+        } else {
+
+    let title = req.body.title;
+    let description = req.body.description;
+    let id = uuid();
+    let done = false;  
+    let values = [id,title,description,done];
+    pool.connect((err,db,done)=>{
+        if(err){
+            return res.status(404).send(err);
+        }
+        else{           
+            db.query('INSERT INTO todo(id,title,description,done) VALUES($1,$2,$3,$4)',[...values],(err,table) =>{
+                done();
+                if(err){
+                    return res.status(404).send(err);
+                }
+                else {
+                    console.log('insert into db');
+                    res.status(200).send({message:'Data inserted!'});
+                }
+            })            
+        }
+    })
+        }
+      });
+
+     }
 }
