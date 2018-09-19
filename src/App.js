5be84369164279d46ac96ac63b743059af278ea8
AMIR SADIQ ALI <amirsadiqalisaifi@gmail.com>
2018-09-19 03:16:59 -0700

    defining logic to get all todos in indexed db

--- src/App.js
+++ src/App.js
@@ -12,6 +12,34 @@ import Axios from "axios";
 
 class App extends Component {
   componentDidMount() {
+    var db;
+    var request = window.indexedDB.open("todoDB", 1);
+    let todos = [];
+    request.onerror = function(event) {
+      console.log("error: ");
+    };
+    let self = this;
+    request.onsuccess = function(event) {
+      db = request.result;
+      var objectStore = db.transaction("todo").objectStore("todo");
+      objectStore.openCursor().onsuccess = function(event) {
+        var cursor = event.target.result;
+        if (cursor) {
+          console.log(cursor.value);
+          todos.push(cursor.value);
+          cursor.continue();
+        } else {
+          self.props.dispatch(getTodos(todos));
+          console.log("todo loaded!");
+        }
+      };
+      console.log("success: " + db);
+    };
+
+    request.onupgradeneeded = function(event) {
+      var db = event.target.result;
+      var objectStore = db.createObjectStore("todo", { keyPath: "id" });
+    };
   }
   render() {
     return (
