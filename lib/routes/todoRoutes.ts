b05d559fd364798dfb0260c525587596b08c336c
Mohsin Virk <mohsinlatifvirk@gmail.com>
2018-09-19 18:53:06 +0500

    change in port

--- lib/routes/todoRoutes.ts
+++ lib/routes/todoRoutes.ts
@@ -6,9 +6,6 @@ const Todo = mongoose.model("todo", TodoSchema);
 
 export class Routes {
   public routes(app): void {
-    app.route("/").get((req: Request, res: Response) => {
-      res.send({ Success: "API is working" });
-    });
     app
       .route("/api/todos")
       .get((req: Request, res: Response) => {
@@ -44,6 +41,15 @@ export class Routes {
 
     app
       .route("/api/todos/:id")
+      .get((req: Request, res: Response) => {
+        Todo.find({ _id: req.params.id })
+          .then(result => {
+            res.status(200).send(result);
+          })
+          .catch(err => {
+            res.status(404).send(err);
+          });
+      })
       .put((req: Request, res: Response) => {
         Todo.findOneAndUpdate(
           { _id: req.params.id },
@@ -53,14 +59,17 @@ export class Routes {
             if (err) {
               res.send(err);
             }
-            res.status(400).json(todo);
+            res.status(200).json(todo);
           }
         );
       })
       .delete((req: Request, res: Response) => {
-        Todo.remove({ _id: req.params.adId })
+        Todo.remove({ _id: req.params.id })
           .then(result => {
-            res.status(200).send(result);
+            res.status(200).send({
+              message: "Item delete with Id",
+              _id: req.params.id
+            });
           })
           .catch(err => {
             res.status(400).send(err);
