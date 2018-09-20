d90ed58175707ff75bc0358b74e9906d55336d24
Hamza-Yousaf <Hamzayousaf933@gmail.com>
2018-09-20 17:13:54 +0500

    add controller file separate db and server

--- lib/routes/todoRoutes.ts
+++ lib/routes/todoRoutes.ts
@@ -1,116 +1,18 @@
 import { Request, Response } from "express";
 import * as uuid from 'uuid/v4'
-let pg = require('pg');
-
-let pool = new pg.Pool({
-  port: 5432,
-  user: 'postgres',
-  password: 'root',
-  host: 'localhost',
-  database: 'react_postgresql_todo',
-  max: 10,
-});
+import { Controller } from "../controller/Controller";
 
 export class Routes {
-  public routes(app): void {
-    app
-      .route("/api/todos")
-      .get((req: Request, res: Response) => {
-        pool.connect((err,db,done)=>{
-          if(err){
-              return res.status(404).send(err);
-          }
-          else{           
-              db.query('select* from todo',(err,table) =>{
-                  done();
-                  if(err){
-                      return res.status(404).send(err);
-                  }
-                  else {         
-                      res.status(200).send(table.rows);
-                  }
-              })            
-          }
-      })
-      })
-      .post((req: Request, res: Response) => {
-        if (!req.body.title) {
-          res.status(400).send({ error: "Please Enter a title" });
-        } else if (!req.body.description) {
-          res.status(400).send({ error: "Please Enter a title" });
-        } else {
-
-    let title = req.body.title;
-    let description = req.body.description;
-    let id = uuid();
-    let done = false;  
-    let values = [id,title,description,done];
-    pool.connect((err,db,done)=>{
-        if(err){
-            return res.status(404).send(err);
-        }
-        else{           
-            db.query('INSERT INTO todo(id,title,description,done) VALUES($1,$2,$3,$4)',[...values],(err,table) =>{
-                done();
-                if(err){
-                    return res.status(404).send(err);
-                }
-                else {
-                    console.log('insert into db');
-                    res.status(200).send({message:'Data inserted!'});
-                }
-            })            
-        }
-    })
-        }
-      });
-
-    app
-      .route("/api/todos/:id")
-      .put((req: Request, res: Response) => {
-        
-        let title = req.body.title;
-        let description = req.body.description;
-        var id = req.params.id;
-        let done = false;  
-        let values = [id,title,description,done];       
-        pool.connect((err,db,done)=>{
-         if(err){
-             return res.status(404).send(err);
-         }
-         else{           
-             db.query('UPDATE todo SET title=($2),description=($3),done=($4) WHERE id=($1)',[...values],(err,table) =>{
-                 done();
-                 if(err){
-                     return res.status(404).send(err);
-                 }
-                 else {         
-                     return res.status(200).send({message: 'update success record'});
-                 }
-             })            
-         }
-     })
-      })
-      .delete((req: Request, res: Response) => {
-
-        var id = req.params.id;
-        console.log(id);
-        pool.connect((err,db,done)=>{
-         if(err){
-             return res.status(404).send(err);
-         }
-         else{           
-             db.query('DELETE FROM todo WHERE id=$1',[id],(err,table) =>{
-                 done();
-                 if(err){
-                     return res.status(404).send(err);
-                 }
-                 else {         
-                     return res.status(200).send({message: 'success in deleting record'});
-                 }
-             })            
-         }
-     })
-      });
+    public todoController: Controller = new Controller();
+    public routes(app): void {
+      app
+        .route("/api/todos")
+        .get(this.todoController.get)
+        .post(this.todoController.post);
+  
+      app
+        .route("/api/todos/:id")
+        .put(this.todoController.update)
+        .delete(this.todoController.delete);
+    }
   }
-}
