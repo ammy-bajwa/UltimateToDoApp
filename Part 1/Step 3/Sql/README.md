03bc8389f82d8d0046d1417277cc9f577b3f20ef
ammy203 <33599273+ammy203@users.noreply.github.com>
2018-09-24 13:37:41 +0500

    recovering test files

--- Part 1/Step 3/Sql/README.md
+++ Part 1/Step 3/Sql/README.md
@@ -1,2 +1,9 @@
 Todo gRPC API Project: A TDD Approach to Building an API using SQL and NoSQL Databases
 By Amir Ali
+
+
+To run test simple run this command
+npm run test
+
+to run server goto server folder and run
+node or nodemon server
---
+++ Part 1/Step 3/Sql/tests/api delete.test.js
@@ -0,0 +1,21 @@
+const grpc = require("grpc");
+const protoPath = require("path").join(__dirname, "../", "proto");
+const proto = grpc.load({ root: protoPath, file: "todo.proto" });
+const client = new proto.myTodos.TodosService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+test("test for removing a todo", () => {
+  client.remove(
+    {
+      todo_id: 279765 //parseInt(Math.random() * 1000000)
+    },
+    (error, response) => {
+      if (!error) {
+        expect(response).not.toBeNull();
+      } else {
+        console.log("Error:", error.message);
+      }
+    }
+  );
+});
