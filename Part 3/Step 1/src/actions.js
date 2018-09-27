ba4741b67f01810a73dbeb2f85f57b3d0d5fc184
AMIR SADIQ ALI <amirsadiqalisaifi@gmail.com>
2018-09-27 18:39:36 +0500

    replacing firebase with firestore

--- Part 3/Step 1/src/actions.js
+++ Part 3/Step 1/src/actions.js
@@ -10,24 +10,25 @@ import * as firebase from "firebase";
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBT76zvy4lpAQLMRStOrvfMgU1Elv3I5Zo",
-  authDomain: "ultimate-todo-app-4c6f6.firebaseapp.com",
-  databaseURL: "https://ultimate-todo-app-4c6f6.firebaseio.com",
-  projectId: "ultimate-todo-app-4c6f6",
-  storageBucket: "ultimate-todo-app-4c6f6.appspot.com",
-  messagingSenderId: "113954197207"
+  apiKey: "AIzaSyAu6lSy3c4vLeqb-dhVIC0Zl5K_czrvLxE",
+  authDomain: "todoappp-1f803.firebaseapp.com",
+  databaseURL: "https://todoappp-1f803.firebaseio.com",
+  projectId: "todoappp-1f803",
+  storageBucket: "todoappp-1f803.appspot.com",
+  messagingSenderId: "587043537304"
 };
 firebase.initializeApp(config);
 const database = firebase.database();
+const firestore = firebase.firestore();
 
 export const getTodos = () => dispatch => {
   let todos = [];
-  database
-    .ref("/")
-    .once("value")
+  firestore
+    .collection("todoApp")
+    .get()
     .then(snapShot => {
       snapShot.forEach(ss => {
-        todos.push(ss.val());
+        todos.push(ss.data());
       });
       dispatch({ type: GET_TODOS, todos });
     })
@@ -38,13 +39,15 @@ export const getTodos = () => dispatch => {
     });
 };
 
-export const postTodo = (formData, history) => dispatch => {
+export const postTodo = formData => dispatch => {
+  console.log(formData);
   let todo = {
     ...formData,
     done: false
   };
-  return database
-    .ref(`/${todo._id}`)
+  return firestore
+    .collection("todoApp")
+    .doc(`${todo._id}`)
     .set(todo)
     .then(() => {
       dispatch({ type: POST_TODO, payload: todo });
@@ -54,12 +57,24 @@ export const postTodo = (formData, history) => dispatch => {
         dispatch({ type: GET_ERRORS, payload: error });
       }
     });
+  // return database
+  //   .ref(`/${todo._id}`)
+  //   .set(todo)
+  // .then(() => {
+  //   dispatch({ type: POST_TODO, payload: todo });
+  // })
+  // .catch(error => {
+  //   if (error) {
+  //     dispatch({ type: GET_ERRORS, payload: error });
+  //   }
+  // });
 };
 export const deleteTodos = _id => dispatch => {
-  return database
-    .ref(`/${_id}`)
-    .remove()
-    .then(payload => {
+  return firestore
+    .collection(`todoApp`)
+    .doc(`${_id}`)
+    .delete()
+    .then(() => {
       dispatch({
         type: DELETE_TODO,
         _id
@@ -73,8 +88,9 @@ export const deleteTodos = _id => dispatch => {
 };
 
 export const updateTodos = (payload, _id) => dispatch => {
-  return database
-    .ref(`/${_id}`)
+  return firestore
+    .collection("todoApp")
+    .doc(`${_id}`)
     .update({ done: payload })
     .then(() => {
       dispatch({
